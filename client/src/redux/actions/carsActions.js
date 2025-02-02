86f84a8a7ef87eaf6faa2904cc8345b74414ace1
goyalom935 <goyalomn935@gmail.com>
2025-02-02 20:55:03 +0530

    fixed

--- client/src/redux/actions/carsActions.js
+++ client/src/redux/actions/carsActions.js
@@ -6,7 +6,7 @@ export const getAllCars = () => async (dispatch) => {
   dispatch({ type: "LOADING", payload: true });
 
   try {
-    const response = await axios.get(`${baseURL}bikes/getallbikes`);
+    const response = await axios.get(`${baseURL}/bikes/getallbikes`);
 
     dispatch({ type: "GET_ALL_CARS", payload: response.data });
     dispatch({ type: "LOADING", payload: false });
